bfc751449f94eaa76786afe6e496e1ed5316d19e
jinah <monsatfri@gmail.com>
2023-08-02 23:31:11 +0900

    회원가입 백엔드 연결 및 디자인, 유효성 검사 수정

--- frontend/src/SignUpForm.jsx
+++ frontend/src/SignUpForm.jsx
@@ -1,35 +1,39 @@
-import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
+import React, { useCallback, useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
 import * as yup from 'yup';
 import './signUpForm.scss';
+import axios from 'axios';
 
 function SignUpForm() {
+  const navigate = useNavigate();
   const formSchema = yup.object({
     name: yup
       .string()
-      .min(2, '이름은 최소 2글자 이상입니다!')
-      .max(10, '이름은 최대 10글자입니다!')
+      .min(2, '⚠️닉네임은 최소 2글자 이상입니다!')
+      .max(10, '⚠️닉네임은 최대 10글자입니다!')
       .matches(
         /^[가-힣a-zA-Z][^!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?\s]*$/,
-        '이름에 숫자와 특수문자를 사용할 수 없습니다.'
+        '⚠️닉네임에 숫자와 특수문자를 사용할 수 없습니다.'
       )
-      .required('이름을 입력해주세요'),
+      .required('⚠️닉네임을 입력해주세요'),
     email: yup
       .string()
       .required('이메일을 입력해주세요.')
-      .email('이메일 형식이 아닙니다.'),
+      .email('⚠️이메일 형식이 아닙니다.'),
     password: yup
       .string()
-      .required('영문, 숫자포함 8자리를 입력해주세요.')
-      .min(8, '최소 8자 이상 가능합니다')
-      .max(15, '최대 15자 까지만 가능합니다')
+      .required('⚠️영문, 숫자포함 8자리를 입력해주세요.')
+      .min(8, '⚠️최소 8자 이상 가능합니다')
+      .max(15, '⚠️최대 15자 까지만 가능합니다')
       .matches(
         /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,15}$/,
-        '영문 숫자포함 8자리를 입력해주세요.'
+        '⚠️영문 숫자포함 8자리를 입력해주세요.'
       ),
     passwordConfirm: yup
       .string()
-      .oneOf([yup.ref('password')], '비밀번호가 다릅니다.'),
+      .oneOf([yup.ref('password')], '⚠️비밀번호가 다릅니다.'),
   });
 
   const {
@@ -42,11 +46,27 @@ function SignUpForm() {
     resolver: yupResolver(formSchema),
   });
 
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = useCallback(
+    (data) => {
+      axios
+        .post('/api/users', data)
+        .then((response) => {
+          console.log(response.data);
+          alert('가입되었습니다!');
+          // 폼 제출 성공 후, BoardList 컴포넌트로 자동으로 이동
+          navigate('/');
+        })
+        .catch((error) => {
+          console.error('회원가입 오류:', error);
+          alert('회원가입 중 오류가 발생했습니다.');
+        });
+    },
+    [navigate]
+  );
 
   return (
     <div className="signUpForm">
-      <div class="container">
+      <div className="container">
         <h1>회원가입</h1>
         <form onSubmit={handleSubmit(onSubmit)}>
           <input name="email" placeholder="이메일" {...register('email')} />
@@ -56,6 +76,7 @@ function SignUpForm() {
             name="password"
             placeholder="비밀번호를 입력해주세요."
             {...register('password')}
+            required
           />
           {errors.password && <p>{errors.password.message}</p>}
           <input
@@ -63,19 +84,21 @@ function SignUpForm() {
             name="passwordConfirm"
             placeholder="비밀번호를 확인해주세요."
             {...register('passwordConfirm')}
+            required
           />
           {errors.passwordConfirm && <p>{errors.passwordConfirm.message}</p>}
-          <input name="name" placeholder="이름" {...register('name')} />
+          <input
+            name="name"
+            placeholder="닉네임"
+            {...register('name')}
+            required
+          />
           {errors.name && <p>{errors.name.message}</p>}
-          <input type="submit" disabled={!isEmpty(errors)} />
+          <input type="submit" id="submitBtn" />
         </form>
       </div>
     </div>
   );
-}
-
-function isEmpty(param) {
-  return Object.keys(param).length === 0;
 }
 
 export default SignUpForm;
